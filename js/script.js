2c90c6ed5316cda3e3cfdf5c9f54f7e973dc2dac
Karine gil <karine-07@hotmail.es>
2025-03-17 19:51:19 +0100

    devolop pelis

--- js/script.js
+++ js/script.js
@@ -1,2 +1,27 @@
 import peliculas from './peliculas.js'
 
+const contenedorAcci =  document.getElementById("genero-28");
+const contenedorThril = document.getElementById("genero-53");
+const contenedorAvent = document.getElementById("genero-12");
+
+const peliAcci = peliculas.filter((pelicula) => pelicula.genre_ids.includes(28));
+const peliThril = peliculas.filter((pelicula) => pelicula.genre_ids.includes(53));
+const peliAvent = peliculas.filter((pelicula) => pelicula.genre_ids.includes(12));
+
+function mostrarPeliculas(listaPeliculas, contenedor) {
+  listaPeliculas.forEach(pelicula => {
+    const div = document.createElement("div");
+    div.className = "pelicula";
+    div.innerHTML = `
+      <img src="https://image.tmdb.org/t/p/w300${pelicula.poster_path}" alt="${pelicula.title}">
+      <h3>${pelicula.title}</h3>`;
+    contenedor.appendChild(div);
+  });
+}
+
+mostrarPeliculas(peliAcci, contenedorAcci);
+mostrarPeliculas(peliThril, contenedorThril);
+mostrarPeliculas(peliAvent, contenedorAvent);
+
+
+
